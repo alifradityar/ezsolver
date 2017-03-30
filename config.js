ab56e99ac867c74b417cfa08a58039a74c1671ab
Alif Raditya Rochman <alifradityar@gmail.com>
2017-03-31 04:28:33 +0700

    fix token

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const config = {
     port: process.env.PORT || 7500,
     wolframalphaToken: process.env.WOLFRAMALPHA_TOKEN || "XQUJKG-VVXQ4W789A",
-    lineToken: process.env.LINE_TOKEN || 'ASDF',
+    lineToken: process.env.LINE_TOKEN || 'U7ff1ca3dc98b470780e5df64baad7fd5',
     papertrailHost: 'logs5.papertrailapp.com',
     papertrailPort: 52498,
     papertrailActive: true,
